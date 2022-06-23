e9ee15e87371ffb07ec2015147de3b6015a4ff90
bensouthgate <bensouthgate@gmail.com>
2022-06-23 13:37:15 +0100

    BN-75 tabset

--- scripts/createDT.R
+++ scripts/createDT.R
@@ -1,19 +1,22 @@
 #' Create DT table with breaks.
-#' 
+#'
 #' @param sample_name Name of sample.
-#' @param res List of results tables. 
+#' @param res List of results tables.
 #' @return list of header, dt table, and breaks.
 createDT <- function(sample_name, res) {
 
   header <- htmltools::tags$h4(sample_name)
 
-  dt <- DT::datatable(res[[sample_name]], class = 'cell-border stripe', extensions = 'Buttons', 
-                      options = list(dom = 'Bfrtip', buttons = c('copy', 'csv', 'excel', 'pdf', 'print')),
-                      filter = "top", 
+  dt <- DT::datatable(res[[sample_name]],
+                      class = 'cell-border stripe',
+                      extensions = 'Buttons',
+                      options = list(dom = 'Bfrtip',
+                                     buttons = c('copy', 'csv', 'excel', 'pdf', 'print')),
+                      filter = "top",
                       width = "100%")
-  
+
   br1 <- htmltools::tags$br()
-  br2 <- htmltools::tags$br()  
+  br2 <- htmltools::tags$br()
   br3 <- htmltools::tags$br()
   br4 <- htmltools::tags$br()
   br5 <- htmltools::tags$br()
@@ -26,52 +29,53 @@ createDT <- function(sample_name, res) {
 }
 
 #' Create DT table with breaks.
-#' 
+#'
 #' @param sample_name Name of sample.
-#' @param res List of results tables. 
+#' @param res List of results tables.
 #' @param unlist If we want to remove sample information and collapse into sample/cluster named list. 
-#' @param html If we want to return html tag output for tagList rendering. 
+#' @param html If we want to return html tag output for tagList rendering.
 #' @param targets targets details for DT table JS.
 #' @return DT datatable OR list of header, DT table, and breaks.
-createDTFea <- function(sample_name, res, unlist = FALSE, html = FALSE, targets = c(11, 12)) {
-  
+createDTFea <- function(sample_name, res, group,
+                        unlist = FALSE, html = FALSE, targets = c(11, 12)) {
+
   # If we want to remove sample information and collapse into sample/cluster named list
   if (unlist) {
 
     res <- unlist(res, recursive = FALSE)
 
     # remove empty elements for clusters without any significant results
-    resbind <- res_gsea_custom[sapply(res, nrow) > 0]
+    resbind <- res[sapply(res, nrow) > 0]
 
     # bind into single results table
-    resbind <- bind_rows(resbind, .id = params$group)
-    
+    resbind <- dplyr::bind_rows(resbind, .id = group)
+
   } else {
 
     # remove empty elements for clusters without any significant results
-    resbind <- res_gsea_custom[sapply(res[[sample_name]], nrow) > 0]
+    resbind <- res[sapply(res[[sample_name]], nrow) > 0]
 
     # bind into single results table
-    resbind <- bind_rows(resbind, .id = params$group)
+    resbind <- dplyr::bind_rows(resbind, .id = group)
   }
 
   # get dt table object
   dt <- plotFeaDT(resbind, pageLength = 5, targets = c(11, 12))
-  
+
   # get html tags from html tools if required for multiple dt tables.
   if (html) {
 
     header <- htmltools::tags$h4(sample_name)
 
     br1 <- htmltools::tags$br()
-    br2 <- htmltools::tags$br()  
+    br2 <- htmltools::tags$br()
     br3 <- htmltools::tags$br()
     br4 <- htmltools::tags$br()
     br5 <- htmltools::tags$br()
     br6 <- htmltools::tags$br()
     br7 <- htmltools::tags$br()
     br8 <- htmltools::tags$br()
-    
+
     dt_complete <- list(header, dt, br1, br2, br3, br4, br5, br6, br7, br8)
 
   } else {
@@ -85,10 +89,10 @@ createDTFea <- function(sample_name, res, unlist = FALSE, html = FALSE, targets 
 }
 
 #' Create DT table with JS length restrictions for FEA.
-#' 
+#'
 #' @param sample_name Results table following FEA.
-#' @param pageLength pageLength. 
-#' @param targets targets for JS to shorten output displayed (suffix ...). 
+#' @param pageLength pageLength.
+#' @param targets targets for JS to shorten output displayed (suffix ...).
 #' @return DT datatable.
 plotFeaDT <- function(resbind, pageLength = 5, targets = c(11, 12)) {
 
